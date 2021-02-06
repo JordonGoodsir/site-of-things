861250aa9f13bf057daa39bd29f77d609d2792d3
JordonGoodsir <jordon.goodsir1@gmail.com>
2021-02-06 17:48:54 +1100

    wheel architecture added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import React, { useReducer } from "react";
 
 import "./styles/styles.scss";
 import HomeController from "./pages/Home/HomeController";
-import Nav from "./components/Nav";
+import Nav from "./components/Nav";  
+import Wheel from "./pages/Wheel/wheel"
+
 
 function App() {
   const initialState = {
@@ -19,7 +21,8 @@ function App() {
       <StateContext.Provider value={{ store, dispatch }}>
         <BrowserRouter> 
         <Nav />
-          <Route exact path="/" component={HomeController} />
+          <Route exact path="/" component={HomeController} />  
+          <Route exact path="/wheel" component={Wheel} /> 
         </BrowserRouter>
       </StateContext.Provider>
     </div>
---
+++ src/pages/Wheel/wheel.js
@@ -0,0 +1,10 @@
+const Wheel = () => {
+  return (
+    <div class="wheel">
+      <h1>wheel</h1>
+      <div class="circle"></div>
+    </div>
+  );
+};
+
+export default Wheel;
