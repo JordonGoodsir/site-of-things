5df075c0611d6514f48de84a534ec7fe4ffdb007
JordonGoodsir <jordon.goodsir1@gmail.com>
2021-02-08 15:18:46 +1100

    wheel design and sectioning initialized

--- src/pages/Wheel/wheel.js
+++ src/pages/Wheel/wheel.js
@@ -1,8 +1,27 @@
 const Wheel = () => {
   return (
-    <div class="wheel">
+    <div>
       <h1>wheel</h1>
-      <div class="circle"></div>
+      
+      <div id="wheel">
+            <div id="inner-wheel">
+                <div class="sec"><span class="fa fa-bell-o"></span></div>
+                <div class="sec"><span class="fa fa-comment-o"></span></div>
+                <div class="sec"><span class="fa fa-smile-o"></span></div>
+                <div class="sec"><span class="fa fa-heart-o"></span></div>
+                <div class="sec"><span class="fa fa-star-o"></span></div>
+                <div class="sec"><span class="fa fa-lightbulb-o"></span></div>  
+            </div>       
+           
+            <div id="spin">
+                <div id="inner-spin"></div>
+            </div>
+            
+        </div>
+        
+        
+        <div id="txt"></div>
+
     </div>
   );
 };
