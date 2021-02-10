4f0c29a571350527459627476cffe659a251dea9
JordonGoodsir <jordon.goodsir1@gmail.com>
2021-02-10 23:11:00 +1100

    alternate wheel

--- src/pages/Wheel/wheel.js
+++ src/pages/Wheel/wheel.js
@@ -4,24 +4,33 @@ const Wheel = () => {
       <h1>wheel</h1>
       
       <div id="wheel">
-            <div id="inner-wheel">
-                <div class="sec"><span class="fa fa-bell-o"></span></div>
-                <div class="sec"><span class="fa fa-comment-o"></span></div>
-                <div class="sec"><span class="fa fa-smile-o"></span></div>
-                <div class="sec"><span class="fa fa-heart-o"></span></div>
-                <div class="sec"><span class="fa fa-star-o"></span></div>
-                <div class="sec"><span class="fa fa-lightbulb-o"></span></div>  
-            </div>       
-           
-            <div id="spin">
-                <div id="inner-spin"></div>
-            </div>
-            
+      <div class="spinner-table"> 
+			<div class="dial">
+				<div class="slice"><div class="label">1</div></div>
+				<div class="slice"><div class="label">2</div></div>
+				<div class="slice"><div class="label">3</div></div>
+				<div class="slice"><div class="label">4</div></div>
+				<div class="slice"><div class="label">5</div></div>
+				<div class="slice"><div class="label">6</div></div>
+				<div class="slice"><div class="label">7</div></div>
+				<div class="slice"><div class="label">8</div></div>
+				<div class="slice"><div class="label">9</div></div>
+				<div class="slice"><div class="label">10</div></div>
+				<div class="slice"><div class="label">11</div></div>
+				<div class="slice"><div class="label">12</div></div>
+			</div>
+		</div>
+		<div class="arrow">
+			<span class="pointer"></span>
+		</div>
+	</div>
+	
+	<div class="content-container">
+		<div class="pre">
+			<button id="spin">Spin</button>
+		</div>
         </div>
         
-        
-        <div id="txt"></div>
-
     </div>
   );
 };
