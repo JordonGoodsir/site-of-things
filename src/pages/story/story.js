443a4d8e61b63970811c8ac04448eb9a0c154974
JordonGoodsir <jordon.goodsir1@gmail.com>
2021-04-28 17:59:58 +1000

    promise all practiced

--- src/pages/story/story.js
+++ src/pages/story/story.js
@@ -1,94 +1,98 @@
 import React, { useState, useEffect } from "react";
 import { useGlobalState } from "../../config/store";
 
-import MakeStory from "./makeStory"; 
-import StoryLoadingAnimation from "./StoryLoadingAnimation"
+import MakeStory from "./makeStory";
+import StoryLoadingAnimation from "./StoryLoadingAnimation";
 import { getWord, getStory, postStory } from "../../services/storyServices";
-import api from "../../config/api"; 
-
+import api from "../../config/api";
 
 const Story = () => {
   const { store, dispatch } = useGlobalState();
-  const { words,storyMenuStatus } = store;
+  const { words, storyMenuStatus } = store;
 
-  let [stories, setStories] = useState([]); 
-  let [loading,setLoading]= useState(false)
+  let [stories, setStories] = useState([]);
+  let [loading, setLoading] = useState(false);
 
   useEffect(() => {
     extractStories();
-
   }, []);
 
-  const openCreateMenu = async () => { 
+  const openCreateMenu = async () => {
+    if (!storyMenuStatus) {
+      setLoading(true);
+      // let tempWordStore = [];
+      // for (let i = 0; i < 5; i++) {
+      //  tempWordStore.push(await getWord());
+      // }
+      // dispatch({
+      //   type: "setWords",
+      //   data: tempWordStore,
+      // });
 
-    if (!storyMenuStatus) {  
-      setLoading(true)
-        let tempWordStore = []; 
-        for (let i = 0; i < 5; i++) {
-         tempWordStore.push(await getWord());
-        }
-        dispatch({
-          type: "setWords",
-          data: tempWordStore,
-        });  
-        setLoading(false)
+      // }
+      await Promise.all(
+        [1, 2, 3, 4, 5].map((v, i) => {
+          return getWord();
+        })
+      )
+        .then((things) => {
+          console.log(things);
+          dispatch({
+            type: "setWords",
+            data: things,
+          });
+        })
+        .catch(console.log);
 
+      setLoading(false);
     }
 
-
-
-if(storyMenuStatus) {  
-  dispatch({
-    type: "setStoryMenuStatus",
-    data: false,
-  }) 
-} else{  
-  console.log("hello")
-  dispatch({
-    type: "setStoryMenuStatus",
-    data: true,
-  }); 
-}
-
-  
+    if (storyMenuStatus) {
+      dispatch({
+        type: "setStoryMenuStatus",
+        data: false,
+      });
+    } else {
+      dispatch({
+        type: "setStoryMenuStatus",
+        data: true,
+      });
+    }
   };
 
   const extractStories = () => {
     getStory() 
     .then((story) => {
       setStories(story);
-    });
+    }) 
+    .catch(console.log())
   };
 
   return (
     <div class="storyPage">
       <h1>Create Your Story</h1>
-      <div>{storyMenuStatus ? <MakeStory /> : ""} 
-      </div> 
-      
+      <div>{storyMenuStatus ? <MakeStory /> : ""}</div>
+
       <div class="storyCardGrid">
-      {stories.map((v, i) => {
-        return (
-          <div class="storyCard">
-            <h2 class="storyTitle"> Title: {v.title}</h2>
-            <h3 class="storyWords">Prompts: {v.wordPrompts.join("  ")}</h3> 
-            <br></br>
-            <p class="storyBody">{v.body}</p>
-          </div>
-        );
-      })} 
-      </div> 
-       
+        {stories.map((v, i) => {
+          return (
+            <div class="storyCard">
+              <h2 class="storyTitle"> Title: {v.title}</h2>
+              <h3 class="storyWords">Prompts: {v.wordPrompts.join("  ")}</h3>
+              <br></br>
+              <p class="storyBody">{v.body}</p>
+            </div>
+          );
+        })}
+      </div>
 
-      {loading? <StoryLoadingAnimation/> : ""}  
-      {/* <StoryLoadingAnimation/> */}
-      <footer> 
-       
-      <button class="makeStoryButton" onClick={openCreateMenu}>Create Story</button>   
-
+      {loading ? <StoryLoadingAnimation /> : ""}
+      <footer>
+        <button class="makeStoryButton" onClick={openCreateMenu}>
+          Create Story
+        </button>
       </footer>
-      
-    </div> 
+    </div>
   );
 };
 
